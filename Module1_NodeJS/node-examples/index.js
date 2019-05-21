41bb66b54b57f72638a6a93a0a5e774d41f880ba
jinwei14 <janviezhang14@gmail.com>
2019-05-21 19:34:01 +0100

    updating learnign files

--- Module1_NodeJS/node-examples/index.js
+++ Module1_NodeJS/node-examples/index.js
@@ -1,8 +1,10 @@
-var rect = {
-  // ES6 style
-  perimeter:(x,y) => (2*(x+y)),
-  area: (x,y) => (x*y)
-};
+// var rect = {
+//   // ES6 style
+//   perimeter:(x,y) => (2*(x+y)),
+//   area: (x,y) => (x*y)
+// };
+
+var rect = require('./rectangle');
 
 function solveRect(l,b){
   console.log("Solving for rectangle with l = " + l + ' and b = '+b);
