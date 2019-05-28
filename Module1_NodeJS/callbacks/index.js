4e2ca00eb577fa7e5a4ad548ca512f1cad6ffbe3
jinwei14 <janviezhang14@gmail.com>
2019-05-28 16:29:24 +0100

    update lecture

--- Module1_NodeJS/callbacks/index.js
+++ Module1_NodeJS/callbacks/index.js
@@ -9,12 +9,16 @@ var rect = require('./rectangle');
 function solveRect(l,b){
   console.log("Solving for rectangle with l = " + l + ' and b = '+b);
 
-  if (l<=0 || b <= 0){
-    console.log('rectangle dimision should be greater than 0');
-  }else{
-    console.log('The area of the rectangle is ' + rect.area(l,b));
-    console.log('the  perimeter of the rectangle is ' + rect.perimeter(l,b));
-  }
+  rect(l,b,(err,rectangle) => {
+    if(err){
+      console.log('Error',err.message);
+    }else{
+      console.log('The area of the rectangle of dimension l = '+l+' and b= '+b+' is '+ rectangle.area())
+      console.log('The perimeter of the rectangle of dimension l = '+l+' and b= '+b+' is '+ rectangle.perimeter())
+    }
+  });
+
+  console.log("this statement is after the call after rect()")
 }
 
 
--- Module1_NodeJS/callbacks/rectangle.js
+++ Module1_NodeJS/callbacks/rectangle.js
@@ -1,16 +1,16 @@
 module.exports = (x, y, callback) => {
   if (x <= 0 || y <= 0) {
-    setTimeOut(() =>
+      setTimeout(() =>
       // callback takes the error and the return value
       //the second parameter will be ignored here
-      callback(new Error('rectangle dimision should be greater than 0 l= ' + x + ' b = ' + y), null),
+      callback(new Error('rectangle dimension should be greater than 0 l= ' + x + ' b = ' + y), null),
       2000);
   } else {
-    setTimeOut(() =>
+      setTimeout(() =>
       callback(null, {
         // this will be passed back 
-        perimeter: (x, y) => (2 * (x + y));
-        area: (x, y) => (x * y);
+        perimeter: () => (2 * (x + y)),
+        area: () => (x * y)
       }),
       2000);
   }
