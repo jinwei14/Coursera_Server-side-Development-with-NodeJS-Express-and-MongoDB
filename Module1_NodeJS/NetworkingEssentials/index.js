fa0e43bd1827438e7ac018ad2d53530f5de81e27
jinwei14 <janviezhang14@gmail.com>
2019-05-31 12:03:52 +0100

    learning http

--- Module1_NodeJS/NetworkingEssentials/index.js
+++ Module1_NodeJS/NetworkingEssentials/index.js
@@ -5,8 +5,16 @@ const hostname = 'localhost';
 
 const port = 3000;
 
+//So the request and response are the two parameters to the function that we supply as a parameter to the createServer method here
 const server = http.createServer((req,res) =>{
+  console.log(req.headers);
 
-
-
+  res.statusCode = 200;
+  res.setHeader('Content-Type','text/html');
+  res.end('<html><body><h1> hello world</h1></body></html>');
 })
+
+
+server.listen(port, hostname, ()=>{
+    console.log(`Server running at http://${hostname}:${port}`);
+});
