58c6e3f79623ab7a62a7782202ba1ea672f9205d
jinwei14 <janviezhang14@gmail.com>
2019-05-31 11:53:43 +0100

    commit http

---
+++ Module1_NodeJS/NetworkingEssentials/index.js
@@ -0,0 +1,12 @@
+//this is an example of how the http works
+const http = require('http');
+
+const hostname = 'localhost';
+
+const port = 3000;
+
+const server = http.createServer((req,res) =>{
+
+
+
+})
