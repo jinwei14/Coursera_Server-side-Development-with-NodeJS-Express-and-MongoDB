e047d51cf86050827d512a456571b0c6be2e2aa8
jinwei14 <janviezhang14@gmail.com>
2019-06-09 16:47:05 +0100

    using morgan and express

--- Module1_NodeJS/Express/index.js
+++ Module1_NodeJS/Express/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const http = require('http');
+// to print the
+const morgan = require('morgan')
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public'));
 
 app.use((req,res,next) =>{
  // next is optional
- console.log(req.headers);
+ // console.log(req.headers);
+ // if we access an non-existing file, the page will jump into this content
  res.statusCode = 200;
  res.setHeader('Content-Type','text/html');
  res.end('<html><body><h1>  this is the Express server </h1></body></html>');
