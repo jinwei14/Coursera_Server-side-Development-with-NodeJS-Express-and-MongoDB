9374527283d745bc29be26be3a3457df01db66de
jinwei14 <janviezhang14@gmail.com>
2019-06-10 16:38:28 +0100

    using express rest

--- Module1_NodeJS/Express/index.js
+++ Module1_NodeJS/Express/index.js
@@ -19,9 +19,47 @@ app.all('/dishes',(req,res,next)=>{
 
 });
 
+//all() will be execute first
 //modification in the all will carried on here
 app.get('/dishes',(req,res,next)=>{
     res.end('Will send all the dishes to you!');
+});
+
+//all() will be execute first
+//The post request coming in from the server will carry some information in the body of the message in the form of JSON data
+app.post('/dishes',(req,res,next)=>{
+    res.end('Will add the dish: ' + req.body.name +
+    ' with details: '+ req.body.description);
+});
+
+
+app.put('/dishes',(req,res,next)=>{
+    res.statusCode = 403;
+    res.end('PUT dishes not supported ');
+});
+
+app.delete('/dishes',(req,res,next)=>{
+    res.end('Deleting all the dishes!');
+});
+
+
+app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name +
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
 });
 
 app.use(express.static(__dirname+'/public'));
