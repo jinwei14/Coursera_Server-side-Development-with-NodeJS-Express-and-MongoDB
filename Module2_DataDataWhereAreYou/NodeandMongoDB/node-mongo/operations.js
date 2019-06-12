f4ed7129921e0939db8d602e7014a1ed512d064d
jinwei14 <janviezhang14@gmail.com>
2019-06-12 16:45:11 +0100

    callback and promise

--- Module2_DataDataWhereAreYou/NodeandMongoDB/node-mongo/operations.js
+++ Module2_DataDataWhereAreYou/NodeandMongoDB/node-mongo/operations.js
@@ -1,41 +1,63 @@
-// This file will encapsulate all that database operations, the four operations that I'm going to perform insert,
-// find, remove and update a document in my database
+// // This file will encapsulate all that database operations, the four operations that I'm going to perform insert,
+// // find, remove and update a document in my database
+//
+// const assert = require('assert');
+//
+// exports.insertDocument = (db, document, collection, callback) => {
+//     const coll = db.collection(collection);
+//     coll.insert(document, (err, result) => {
+//         assert.equal(err, null);
+//         console.log("Inserted " + result.result.n +
+//             " documents into the collection " + collection);
+//         callback(result);
+//     });
+// };
+//
+// exports.findDocuments = (db, collection, callback) => {
+//     const coll = db.collection(collection);
+//     coll.find({}).toArray((err, docs) => {
+//         assert.equal(err, null);
+//         callback(docs);
+//     });
+// };
+//
+// exports.removeDocument = (db, document, collection, callback) => {
+//     const coll = db.collection(collection);
+//     coll.deleteOne(document, (err, result) => {
+//         assert.equal(err, null);
+//         console.log("Removed the document ", document);
+//         callback(result);
+//     });
+// };
+//
+// exports.updateDocument = (db, document, update, collection, callback) => {
+//     const coll = db.collection(collection);
+//     //second argument is which field will be updated
+//     coll.updateOne(document, { $set: update }, null, (err, result) => {
+//         assert.equal(err, null);
+//         console.log("Updated the document with ", update);
+//         callback(result);
+//     });
+// };
 
 const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Inserted " + result.result.n +
-            " documents into the collection " + collection);
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the document ", document);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    //second argument is which field will be updated
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document with ", update);
-        callback(result);
-    });
+    return coll.updateOne(document, { $set: update }, null);
 };
