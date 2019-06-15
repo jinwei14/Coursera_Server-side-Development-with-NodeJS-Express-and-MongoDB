7a642b1ed3292bc7e8996ca712767a870e9784f0
jinwei14 <janviezhang14@gmail.com>
2019-06-15 10:43:47 +0100

    mongoose

--- Module2_DataDataWhereAreYou/MongooseODM/node-mongoose/index.js
+++ Module2_DataDataWhereAreYou/MongooseODM/node-mongoose/index.js
@@ -8,50 +8,68 @@ const connect = mongoose.connect(url);
 // this is using promise
 connect.then((db) => {
 
-    console.log('Connected correctly to server');
+  console.log('Connected correctly to server');
 
-    Dishes.create({
-            name: 'Uthapizza',
-            description: 'Test'
-        })
-        .then((dish) => {
-            console.log(dish);
+  Dishes.create({
+      name: 'Uthappizza',
+      description: 'test'
+    })
+    .then((dish) => {
+      console.log(dish);
 
-            return Dishes.find({}).exec();
+      return Dishes.findByIdAndUpdate(dish._id, {
+          $set: {
+            description: 'Updated test'
+          }
+        }, {
+          new: true
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .exec();
+    })
+    .then((dish) => {
+      console.log(dish);
 
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+      dish.comments.push({
+        rating: 5,
+        comment: 'I\'m getting a sinking feeling!',
+        author: 'Leonardo di Carpaccio'
+      });
 
-    // var newDish = Dishes({
-    //     name: 'Uthappizza',
-    //     description: 'test'
-    // });
-    //
-    // newDish.save()
-    //     .then((dish) => {
-    //         console.log(dish);
-    //
-    //         return Dishes.find({});
-    //     })
-    //     .then((dishes) => {
-    //         console.log(dishes);
-    //
-    //         return Dishes.remove({});
-    //     })
-    //     .then(() => {
-    //         return mongoose.connection.close();
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //     });
+      return dish.save();
+    })
+    .then((dish) => {
+      console.log(dish);
+
+      return Dishes.remove({});
+    })
+    .then(() => {
+      return mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // var newDish = Dishes({
+  //     name: 'Uthappizza',
+  //     description: 'test'
+  // });
+  //
+  // newDish.save()
+  //     .then((dish) => {
+  //         console.log(dish);
+  //
+  //         return Dishes.find({});
+  //     })
+  //     .then((dishes) => {
+  //         console.log(dishes);
+  //
+  //         return Dishes.remove({});
+  //     })
+  //     .then(() => {
+  //         return mongoose.connection.close();
+  //     })
+  //     .catch((err) => {
+  //         console.log(err);
+  //     });
 
 });
