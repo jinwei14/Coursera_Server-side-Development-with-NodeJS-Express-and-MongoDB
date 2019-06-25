ffd35b0665613395322af9eb408e5a77256be0b8
jinwei14 <janviezhang14@gmail.com>
2019-06-25 18:27:45 +0100

    auth by cookie

--- Week3_WhoGoesThere/conFusionServer/app.js
+++ Week3_WhoGoesThere/conFusionServer/app.js
@@ -45,32 +45,73 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(cookieParser('12345-67890-09876-54321'));
 
-function auth(req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  // if the authorization not existed
-  if (!authHeader) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-    return;
-  }
+//  // using cookies video1
+// function auth(req, res, next) {
+//   console.log(req.headers);
+//   var authHeader = req.headers.authorization;
+//   // if the authorization not existed
+//   if (!authHeader) {
+//     var err = new Error('You are not authenticated!');
+//     res.setHeader('WWW-Authenticate', 'Basic');
+//     err.status = 401;
+//     next(err);
+//     return;
+//   }
+//
+//   // if the authorization existed and split the buffer into 2
+//   var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+// // username and password
+//   var user = auth[0];
+//   var pass = auth[1];
+//   if (user == 'admin' && pass == 'password') {
+//     next(); // authorized
+//   } else {
+//     var err = new Error('You are not authenticated!');
+//     res.setHeader('WWW-Authenticate', 'Basic');
+//     err.status = 401;
+//     next(err);
+//   }
+// }
 
-  // if the authorization existed and split the buffer into 2
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-// username and password
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-    next(); // authorized
-  } else {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
+function auth (req, res, next) {
+  console.log(req.signedCookies);
+
+  // So we are saying if the signed cookie doesn't contain the user property on it,
+  // then we expect the user to authorize by including the authorization header
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        res.cookie('user','admin',{signed: true});
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+    }
+  }
+  else {
+      if (req.signedCookies.user === 'admin') {
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
   }
 }
+
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
